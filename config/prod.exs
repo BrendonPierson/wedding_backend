47145aebfd30bc503f506b2287e61baa9a1c93eb
Brendon Pierson <BrendonPierson@gmail.com>
2017-06-17 09:10:05 -0500

    Set port

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :special_day, SD.Web.Endpoint,
   on_init: {SD.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
+  http: [port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
