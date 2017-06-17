57021041dbbf087d744a270a4d176cadbe5cbcd0
Brendon Pierson <BrendonPierson@gmail.com>
2017-06-17 15:52:50 -0500

    Tweaked endpoint settings

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,7 @@ use Mix.Config
 config :special_day, SD.Web.Endpoint,
   on_init: {SD.Web.Endpoint, :load_from_system_env, []},
   http: [port: 4000],
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
